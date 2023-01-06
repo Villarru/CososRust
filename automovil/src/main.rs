1ff91999c7b509d2f90ad79651531e25061ca26a
Villarru <ljvmg11@gmail.com>
2023-01-05 21:46:26 -0800

    se agregaron condicionales a automovil

--- automovil/src/main.rs
+++ automovil/src/main.rs
@@ -1,42 +1,41 @@
 // Usar enum, struct y fn para poder procesar información de tres automóviles nuevos.
-
-struct Auto {
-    color: String,
-    transmision: Transmision,
-    convertible: bool,
-    kilometraje: u32,
-}
-
+struct Auto {color: String, motor: Transmision, techo: bool, estado: (Estado, u32),}
 #[derive(PartialEq, Debug)]
-enum Transmision{
-    Manual,
-    SemiAutom,
-    Automatica,
+enum Transmision{Manual, SemiAuto, Automatica,}
+#[derive(PartialEq, Debug)]
+enum Estado {Nuevo, Usado,}
+
+fn calidad (kilometraje: u32) -> (Estado,u32){
+    if kilometraje > 0 {return (Estado::Usado, kilometraje)}
+    (Estado::Nuevo, kilometraje)
 }
 
-fn fabrica_auto(color: String, transmision: Transmision, convertible: bool) -> Auto{
-    let auto = Auto{color, transmision, convertible, kilometraje: 0, };
-    auto
+fn fabrica_auto(num:i32, color: String, motor: Transmision, techo: bool, kilometraje: u32){
+    let carro = Auto{color, motor, techo, estado: calidad(kilometraje)};
+    imprimir_auto(&carro,num);
 }
+
+fn imprimir_auto(carro: &Auto, num: i32){
+    let mut techo = String::from("");
+    if carro.techo { techo = String::from("Es convertible\n ");}
+    let (estado, km): &(Estado, u32) =  &carro.estado;
+    
+    hr();
+    println!(" --- Auto {} --- \n Color: {}\n Motor: {:?}\n {}Estado {:?}\n Kilometraje: {}",
+             num, &carro.color, &carro.motor, techo, estado, km);
+}
+
 fn hr(){
     println!("=============================");
 }
 
-fn imprimir_auto(carro: &Auto, num: i32){
-    hr();
-    println!(" --- Auto {} --- \n Color: {}\n Trasmision: {:?}\n Convertible: {}\n Kilometraje: {}",
-             num, carro.color, carro.transmision, carro.convertible, carro.kilometraje);
-}
-
 fn main() {
-    let mut carro: Auto = fabrica_auto(String::from("Verde"),Transmision::Automatica, false);
-    imprimir_auto(&carro,1);
-    
-    carro = fabrica_auto(String::from("Negro"),Transmision::Manual, true);
-    imprimir_auto(&carro,2);
-    
-    carro = fabrica_auto(String::from("Anaranjado"),Transmision::SemiAutom, false);
-    imprimir_auto(&carro,3);
+    fabrica_auto(1, String::from("Verde"),Transmision::Automatica, false, 0);
 
+    fabrica_auto(2, String::from("Negro"),Transmision::Manual, true, 0);
+
+    fabrica_auto(3, String::from("Anaranjado"),Transmision::SemiAuto, false, 125);
+
+    fabrica_auto(4, String::from("Morado"),Transmision::Automatica, true, 2560);
     hr();
 }
